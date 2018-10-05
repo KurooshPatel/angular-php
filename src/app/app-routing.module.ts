3ce09668f246d4b5f6308639e41af8c7d70d0bb8
preeti-bendal <preeti.bendal@arsenaltech.com>
2018-10-05 09:36:50 +0530

    Login service updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,9 @@ import { AppComponent } from './app.component';
 // import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'user/profile', loadChildren : './components/login/login.module#LoginModule'},
-  { path: 'login', loadChildren : './components/login/login.module#LoginModule'}
+  { path: 'login', loadChildren : './components/login/login.module#LoginModule'},
 ];
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
